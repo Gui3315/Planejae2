8bcb19b786796a840d5d7f9ffe94fa4761fb2051
Guilherme Fernandes <guilherme_thegame@hotmail.com>
2025-08-14 14:58:54 -0300

    Adição das configurações PWA para o site

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,7 +12,19 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
   react(),
-],
+  VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Planejaê',
+        short_name: 'Planejaê',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#0f172a',
+        theme_color: '#2563eb',
+        icons: [
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
+        ],},},),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
